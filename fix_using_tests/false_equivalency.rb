c7f9026628a7ffd534313f7fd5fb1607b53148e8
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-26 20:59:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,9 +11,9 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
 end
@@ -21,5 +21,4 @@ end
 def runner
   prompt_user
   selection(get_user_input)
-  binding.pry
 end
